6a8d8fb43431d1939c7e99666949a0348774f362
Ryan McAlpine <mcalpine06@outlook.com>
2024-05-08 21:41:42 +1000

    Finished latest_coin_market_price \ Start latest_buy_price \ Re-did some docstrings

--- src/coinspot-wrapper/coinspot.py
+++ src/coinspot-wrapper/coinspot.py
@@ -3,13 +3,11 @@ import requests
 PUB_API_BASE_URL = "https://www.coinspot.com.au/pubapi/v2" 
 
 class PublicCoinspotAPI():
-    """Gives access to all Coinspot API
+    """Gives access to all public Coinspot API
     """
     def latest_prices(self) -> requests.Response:
         """
-        status - ok, error
-        message - ok, description of error if error occurred
-        prices - array of objects with one set of properties for each coin with latest buy and sell prices, non aud markets are symbolised by (e.g.) 'btc_usdt'
+        https://www.coinspot.com.au/v2/api#latestprices
         
         Returns:
             requests.Response: A response containing all coin price data
@@ -17,19 +15,37 @@ class PublicCoinspotAPI():
         
         return requests.get(f"{PUB_API_BASE_URL}/latest")
     
-    def latest_coin_prices(self, coin) -> requests.Response:
-        """Takes coin name and returns API response
+    def latest_coin_price(self, coin) -> requests.Response:
+        """https://www.coinspot.com.au/v2/api#latestpricescoin
         
-        status - ok, error
-        message - ok, description of error if error occurred
-        prices - object with set of properties for coin with latest buy, ask and last prices
-
-
         Args:
-            coin (string): Coin short term
+            coin (string): coin short name, example value 'BTC', 'LTC', 'DOGE'
 
         Returns:
             requests.Response: A response containing coin price data
         """
         
         return requests.get(PUB_API_BASE_URL + "/latest/" + coin)
+    
+    def latest_coin_market_price(self, coin, markettype) -> requests.Response:
+        """https://www.coinspot.com.au/v2/api#latestpricescoinmarket
+
+        Args:
+            coin (string): coin short name, example value 'BTC', 'LTC', 'DOGE'
+            markettype (string): market coin short name, example value 'USDT' (only for available markets)
+
+        Returns:
+            requests.Response: object with set of properties for coin with latest buy, ask and last prices 
+        """
+        
+        return requests.get(PUB_API_BASE_URL + "/latest" + f"/{coin}/{markettype}")
+    
+    def latest_buy_price(self, coin) -> requests.Response:
+        """https://www.coinspot.com.au/v2/api#latestbuyprice
+
+        Args:
+            coin (string): coin short name, example value 'BTC', 'LTC', 'DOGE'
+
+        Returns:
+            requests.Response: _description_
+        """
